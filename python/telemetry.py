f218adc9bcdbe9e59b2f6e7bcdc2454f6d6c7912
Brian Webb <webbbn@gmail.com>
2019-10-18 09:30:31 -0700

    Adds RC control to MSP interface.

--- python/telemetry.py
+++ python/telemetry.py
@@ -3,6 +3,8 @@ import math
 import time
 import queue
 import threading
+import socket
+import struct
 import pymavlink.mavutil as mavutil
 from pymavlink.dialects.v10 import ardupilotmega as mavlink2
 from MultiWii import MultiWii
@@ -10,11 +12,14 @@ from MultiWii import MultiWii
 class SerialTelemetryRx(object):
     """Receive telemetry over a standard UART connection"""
 
-    def __init__(self, uart = "/dev/ttyS0", baudrate = 115200, protocol = "msp"):
+    def __init__(self, protocol = "msp", uart = "/dev/ttyS1", baudrate = 115200,
+                 rc_host = None, rc_port = 14551):
         self.uart = uart
         self.baudrate = baudrate
         self.done = False
-        self.mavs = mavutil.mavlink_connection(uart, baud=baudrate)
+        self.rc_host = rc_host
+        self.rc_port = rc_port
+        self.rc_chan = None
 
         if protocol == "msp":
 
@@ -29,46 +34,81 @@ class SerialTelemetryRx(object):
             self.thread = threading.Thread(target = self.start_msp)
 
         elif protocol == "mavlink":
+            self.mavs = mavutil.mavlink_connection(uart, baud=baudrate)
             self.thread = threading.Thread(target = self.start_mavlink)
         else:
             raise Exception("Unsupported telemetry format: " + protocol)
 
-        # Start the processing thread
+        # Create the RC receive thread
+        if rc_host:
+            self.recv_thread = threading.Thread(target = self.recv_rc)
+        else:
+            self.recv_thread = None
+
+        # Start the processing threads
         self.thread.start()
+        self.recv_thread.start()
 
     def __del__(self):
         self.done = True
-        self.thread.join()
+        self.join()
 
     def join(self):
         self.thread.join()
+        if self.recv_thread:
+            self.recv_thread.start()
 
     def start_msp(self):
+
         counter = 0
         while not self.done:
-            # High frequency messages
-            att = self.mw.getAttitude()
-            misc = self.mw.getMisc()
 
-            # Send the attitude message
-            roll = float(att["angx"]) * math.pi / 180.0
-            pitch = float(att["angy"]) * math.pi / 180.0
-            yaw = float(att["heading"]) * math.pi / 18.0
-            rollspeed = 0; # rad/s
-            pitchspeed = 0; # rad/s
-            yawspeed = 0; # rad/s
-            self.mavdown.mav.attitude_send(0, roll, pitch, yaw, rollspeed, pitchspeed, yawspeed)
+            # High frequency messages
+            if counter % 10:
+                att = self.mw.getAttitude()
+                misc = self.mw.getMisc()
+
+                # Send the attitude message
+                roll = float(att["angx"]) * math.pi / 180.0
+                pitch = float(att["angy"]) * math.pi / 180.0
+                yaw = float(att["heading"]) * math.pi / 18.0
+                rollspeed = 0; # rad/s
+                pitchspeed = 0; # rad/s
+                yawspeed = 0; # rad/s
+                self.mavdown.mav.attitude_send(0, roll, pitch, yaw, rollspeed, pitchspeed, yawspeed)
             
             # Low frequency messages
-            if counter % 10:
+            if counter % 100:
 
                 # Send a heartbeat message
                 self.mavdown.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
                                                 mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0)
 
-            # Loop with a period of 100ms
+            # Update the RC channels if available
+            if self.rc_chan:
+                self.mw.setRC(self.rc_chan)
+                self.rc_chan = None
+
+            # Loop with a period of 10ms
             counter += 1
-            time.sleep(0.1)
+            time.sleep(0.01)
+
+    def recv_rc(self):
+
+        # Create the receive socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Find to the receive port
+        sock.bind((self.rc_host, self.rc_port))
+
+        # Receive RC messages off the UDP port
+        while True:
+
+            # Receive the next RC message
+            data, addr = sock.recvfrom(1024)
+
+            # Unpack the channels
+            self.rc_chan = struct.unpack("<HHHHHHHHHHHHHHHH", data)
 
     def start_mavlink(self):
         while not self.done:
@@ -122,7 +162,7 @@ class UDPTelemetryTx(object):
 class SerialTelemetryTx(object):
     """Seld telemetry over a standard UART connection"""
 
-    def __init__(self, queue, uart = "/dev/ttyS0", baudrate = 57600):
+    def __init__(self, queue, uart = "/dev/ttyS1", baudrate = 57600):
         self.queue = queue
         self.uart = uart
         self.baudrate = baudrate
@@ -142,6 +182,5 @@ class SerialTelemetryTx(object):
                 self.queue.put(msg)
 
 if __name__ == '__main__':
-    queue = queue.Queue()
-    telem = SerialTelemetryRx(queue, uart="/dev/ttyS1")
-    time.sleep(20)
+    telem = SerialTelemetryRx(rc_host="127.0.0.1")
+    telem.join()
